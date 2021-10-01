38e0c16f66f049f9b560d40b54f41ca55c1173d7
Mounir Kalai <57255735+Mounir070@users.noreply.github.com>
2021-10-01 09:42:58 +0000

    rode slang

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ function setup() {
   frameRate(10);
 }
 
-var [xpos, ypos, xspeed, yspeed] = [225, 225, 0, 0];
+var [xpos, ypos, xspeed, yspeed] = [160, 160, 0, 0];
 
 function draw() {
-	background(100);
+	background(110);
 	
 	fill(0);
 	
-	fill(0, 255, 0);
-	rect(xpos, ypos, 25, 25);
+	fill(220 ,0 , 0);
+	rect(xpos, ypos, 20, 20);
 	
 	if(xpos >= 0 && xpos + 50 <= 500) xpos += xspeed;
 	if(ypos >= 0 && ypos + 50 <= 500) ypos += yspeed;
