4947f4bd69d869fd15da7809b0e67564a4764596
Mounir Kalai <57255735+Mounir070@users.noreply.github.com>
2021-10-04 12:05:58 +0000

    food and loopje gemaakt

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ var scl = 20;
 function setup() {
   createCanvas(500, 400);
   snake = new Snake();
-  frameRate(7);
-
+  food = new Food();
 }
+
 class Snake {
   constructor() {
     this.x = width / 2;
@@ -15,28 +15,45 @@ class Snake {
   }
 
   draw() {
-
+    if (xpos >= 0 && xpos + 50 <= 500) xpos += xspeed;
+    if (ypos >= 0 && ypos + 50 <= 500) ypos += yspeed;
   }
 }
 
-class food{
- constructor(){
-   thiis.x = random.width / 2
- }
+class Food {
+  constructor() {
+    this.x = random(width)
+    this.y = random(height)
+  }
 
+  draw() {
+    fill(220, 0, 0);
+    rect(this.x, this.y, 20, 20);
+  }
 }
+
 var [xpos, ypos, xspeed, yspeed] = [160, 160, 0, 0];
+
+var foods = [];
 
 function draw() {
   background(110);
+ 
+  if(frameCount % 100 == 0){
+    console.log("new!");
+    foods.push(new Food());
+    if(foods.length > 6){
+      foods.splice(0,1);
+    }
+  }
 
   fill(0);
 
-  fill(220, 0, 0);
-  rect(xpos, ypos, 20, 20);
+  foods.forEach((f) => {
+    f.draw();
+  });
 
-  if (xpos >= 0 && xpos + 50 <= 500) xpos += xspeed;
-  if (ypos >= 0 && ypos + 50 <= 500) ypos += yspeed;
+
 }
 
 
